69bb4038df6f72de9ff727733704aa05b1358b9e
Nama GitHub Kamu <161846672+yeszi@users.noreply.github.com>
2026-02-11 22:30:18 +0700

    perbaiki api link

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './assets/main.css' // (Opsional, jika baris ini error, hapus saja)
+import './assets/main.css' 
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router' // Pastikan baris ini ada!
+import router from './router' 
 
 const app = createApp(App)
 
-app.use(router) // Pastikan baris ini ada!
+app.use(router) 
 app.mount('#app')
